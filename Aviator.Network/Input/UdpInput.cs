be4b834133f75a989c30d0ff01671b49c81602a6
Martin Paul <martin.paul@vogler-engineering.de>
2025-02-27 08:03:38 +0100

    udp bigger buffer fix for some weired acars json

--- Aviator.Network/Input/UdpInput.cs
+++ Aviator.Network/Input/UdpInput.cs
@@ -10,7 +10,8 @@ public class UdpInput(string host, int port) : IInput
     public async Task ReceiveAsync(InputHandler onReceive, CancellationToken cancellationToken = default)
     {
         using var udpClient = new UdpClient(new IPEndPoint(IPAddress.Parse(host), port));
-
+        udpClient.Client.ReceiveBufferSize = ushort.MaxValue;
+        
         while (!cancellationToken.IsCancellationRequested)
         {
             var datagram = await udpClient.ReceiveAsync(cancellationToken).ConfigureAwait(false);
