bdad3ef8a925ba88f956ba75eb420aeea4acc99b
Martin Paul <martin.paul-3277@web.de>
2025-03-22 17:38:15 +0100

    fix

--- Aviator.Main/DependencyInjection/InfluxDbExtension.cs
+++ Aviator.Main/DependencyInjection/InfluxDbExtension.cs
@@ -10,22 +10,23 @@ public static class InfluxDbExtension
 {
     public static WebApplicationBuilder AddAviatorInfluxDb(this WebApplicationBuilder builder)
     {
-        var influxConfig = builder.Configuration.GetSection(MetricsConfig.Section);
+        var influxConfig = builder.Configuration.GetSection(MetricsConfig.Section).GetSection("InfluxDB").Get<InfluxDbConfig>();
+        ArgumentNullException.ThrowIfNull(influxConfig);
         
-        // var client = new InfluxDBClient(new ClientConfig
-        // {
-        //     Host = influxConfig.Url,
-        //     Organization = influxConfig.Organization,
-        //     Database = influxConfig.Bucket,
-        //     Token = influxConfig.Token,
-        //     WriteOptions = new WriteOptions
-        //     {
-        //         Precision = WritePrecision.S
-        //     },
-        //     Timeout = TimeSpan.FromSeconds(60)
-        // });
-        //
-        // builder.Services.AddSingleton(client);
+        var client = new InfluxDBClient(new ClientConfig
+        {
+            Host = influxConfig.Url,
+            Organization = influxConfig.Organization,
+            Database = influxConfig.Bucket,
+            Token = influxConfig.Token,
+            WriteOptions = new WriteOptions
+            {
+                Precision = WritePrecision.S
+            },
+            Timeout = TimeSpan.FromSeconds(60)
+        });
+        
+        builder.Services.AddSingleton(client);
         
         return builder;
     }
