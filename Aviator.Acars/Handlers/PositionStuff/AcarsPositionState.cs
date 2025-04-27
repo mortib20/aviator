acfbed7708c3e64e61a91a50a732161c7380bf4c
Martin Paul <martin.paul-3277@web.de>
2025-04-27 18:08:56 +0200

    delete positions after 1 day

--- Aviator.Acars/Handlers/PositionStuff/AcarsPositionState.cs
+++ Aviator.Acars/Handlers/PositionStuff/AcarsPositionState.cs
@@ -2,14 +2,31 @@
 
 namespace Aviator.Acars.Handlers.PositionStuff;
 
-public class AcarsPositionState(IHubContext<AcarsHub> acarsHub)
+public class AcarsPositionState
 {
-    public List<Position> Positions { get; } = [];
+    private Timer _deleteTimer;
+    private readonly IHubContext<AcarsHub> _acarsHub;
+
+    public AcarsPositionState(IHubContext<AcarsHub> acarsHub)
+    {
+        _acarsHub = acarsHub;
+        _deleteTimer = new Timer(DeleteOldPositions);
+        _deleteTimer.Change(TimeSpan.Zero, TimeSpan.FromSeconds(10));
+    }
+
+    public List<Position> Positions { get; set; } = [];
 
     public async Task AddPositionAsync(Position position, CancellationToken cancellationToken = default)
     {
         Positions.Add(position);
 
-        await acarsHub.Clients.Group("Position").SendAsync("Position", position, cancellationToken: cancellationToken).ConfigureAwait(false);
+        await _acarsHub.Clients.Group("Position").SendAsync("Position", position, cancellationToken: cancellationToken).ConfigureAwait(false);
+    }
+
+    private void DeleteOldPositions(object? o)
+    {
+        var t = Positions.Where(p => p.DateTime > DateTimeOffset.UtcNow.Subtract(TimeSpan.FromDays(1)));
+
+        Positions = t.ToList();
     }
 }
