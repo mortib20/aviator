ad23ae329fcb44f6f595e44939e779e709c189f5
Martin Paul <martin.paul-3277@web.de>
2025-08-12 20:35:44 +0200

    Maybe also add the send stuff

--- Aviator.Airframe/Metrics/AirframeMetrics.cs
+++ Aviator.Airframe/Metrics/AirframeMetrics.cs
@@ -1,12 +1,25 @@
 ﻿using Aviator.Global.Metrics.Implementation;
 using InfluxDB3.Client.Write;
+using QuestDB;
+using QuestDB.Utils;
 
 namespace Aviator.Airframe.Metrics;
 
-public class AirframeMetrics(InfluxDbMetrics influxDbMetrics)
+public class AirframeMetrics(InfluxDbMetrics influxDbMetrics, SenderOptions senderOptions)
 {
     public async Task HandleAirframe(Frames.Entities.Airframe airframe)
     {
+        using var sender = Sender.New(senderOptions);
+
+        await sender.Table("airframes")
+            .Symbol("Channel", airframe.Channel)
+            .Symbol("FrameType", airframe.FrameType.ToString())
+            .Symbol("ProtocolType", airframe.ProtocolType.ToString())
+            .Column("value", 1)
+            .AtAsync(DateTime.UtcNow);
+
+        await sender.SendAsync();
+        
         var point = PointData.Measurement("airframes")
             .SetTag("channel", airframe.Channel)
             .SetTag("frameType", airframe.FrameType.ToString())
